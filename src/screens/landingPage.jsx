1bc5ed594b84c5142e1eb18f7e767d32161748a7
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2023-11-24 09:05:29 +0530

    features added to landing page

--- src/screens/landingPage.jsx
+++ src/screens/landingPage.jsx
@@ -5,12 +5,14 @@ import ProductCard from '../components/ProductCard'
 import Hero from '../components/landing-page-components/hero'
 import Footer from '../components/footerMain'
 import Intro from '../components/landing-page-components/intro'
+import Features from '../components/landing-page-components/features'
 export default function landingPage() {
   return (
     <>
     <NavBar> </NavBar>
     <Hero></Hero>
     <Intro></Intro>
+    <Features></Features>
     <Footer></Footer>
 
     </>
